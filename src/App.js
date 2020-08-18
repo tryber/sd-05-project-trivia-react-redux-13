bdf22dd51fbea26d4f86f319e1e2f9d3de88b11a
Elisa <elisatcamillo@gmail.com>
2020-08-18 18:40:47 -0300

    arquivos iniciais

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import logo from './trivia.png';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Feedback from './Pages/Feedback';
+import Game from './Pages/Game';
+import Login from './Pages/Login';
+import Settings from './Pages/Settings';
+import Ranking from './Pages/Ranking';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" Component={Login} />
+          <Route exact path="/game" Component={Game} />
+          <Route exact path="/settings" Component={Settings} />
+          <Route exact path="/ranking" Component={Ranking} />
+          <Route exact path="/feedback" Component={Feedback} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
