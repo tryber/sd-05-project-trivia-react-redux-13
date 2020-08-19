92a8092abee9eb568df0cedac6b04d53bfe690c9
Elisa <elisatcamillo@gmail.com>
2020-08-19 20:05:56 -0300

    base da páágina inicial

--- src/Pages/Settings.js
+++ src/Pages/Settings.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 
 export default class Settings extends Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        <h1 data-testid="settings-title">Configurações</h1>
+      </div>
+    );
   }
 }
 
