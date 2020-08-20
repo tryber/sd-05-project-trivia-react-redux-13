70b260aee7c8849dff58504eb612bf6121bf7a84
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-08-20 18:59:35 -0300

    Informações header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
-// import { connect } from 'react-redux';
+import { fetchGravatar } from '../actions/requestGravatar.js';
+import { connect } from 'react-redux';
 
-export default class Header extends Component {
+class Header extends Component {
+  componentDidMoutn() {
+    const { fetchGravatar } = this.props;
+    fetchGravatar();
+  }
   render() {
-    return <div />;
+    const { profilePicture, name, score } = this.props.gravatarReducer;
+    return <img src={`${profilePicture}`} />;
   }
 }
 
-// const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => ({
+  gravatarReducer:{
+    name: state.gravatarReducer.name,
+    score: state.gravatarReducer.score,
+    profilePicture: state.gravatarReducer.profilePicture,
+  }
+});
 
-// const mapDispatchToProps = {};
+const mapDispatchToProps = (dispatch) => ({
+  fetchGravatar: (result) => dispatch(fetchGravatar(result)),
+});
 
-// export default connect(mapStateToProps, mapDispatchToProps)(Header);
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
+
+// A imagem do perfil vinda do Gravatar em um elemento que deve possuir o atributo data-testid com o valor header-profile-picture
+// O nome da pessoa em um elemento que deve possuir o atributo data-testid com o valor header-player-name
+// O placar zerado em um elemento que deve possuir o atributo data-testid com o valor header-score
