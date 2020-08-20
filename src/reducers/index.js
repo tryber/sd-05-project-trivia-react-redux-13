92beea0d5272c53d66790cb85d7e6946c9be3df6
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-08-20 16:36:08 -0300

    Add reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,30 @@
+import { REQUEST_GRAVATAR, RECEIVED_GRAVATAR, FAILED_GRAVATAR } from '../actions/requestGravatar';
 
+const INITIAL_STATE = [
+  {
+    isFetching: false,
+    profilePicture: [],
+    error: '',
+  },
+];
+
+export default function gravatarReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case REQUEST_GRAVATAR:
+      return {
+        ...state,
+        isFetching: true,
+      };
+    case RECEIVED_GRAVATAR:
+      return {
+        ...state,
+        profilePicture: [...state, action.data],
+        isFetching: false,
+      };
+    case FAILED_GRAVATAR:
+      return {
+        ...state,
+        error: action.error,
+      };
+  }
+}
