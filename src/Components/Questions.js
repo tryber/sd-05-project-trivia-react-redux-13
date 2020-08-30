ed99db3a497ba43908e49ec32ad26be7832fefb9
System Administrator <root@MacBook-Pro-de-Lucas.local>
2020-08-30 16:01:59 -0300

    codeclimate

--- src/Components/Questions.js
+++ src/Components/Questions.js
@@ -46,6 +46,8 @@ Questions.propTypes = {
         picture: propTypes.string,
       },
     ],
+    category: propTypes.string,
+    questions: propTypes.string,
     token: propTypes.string,
   }).isRequired,
 };
